919edbfa94f45cfaec7b9720976b91fe0f63cab7
Kalle <domare2@hotmail.com>
2021-05-04 03:14:39 +0200

    userLogin feature

--- public_html/UserLogin.js
+++ public_html/UserLogin.js
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 //array för att testa rätt/fel login input
+//Kommer befinna sig i en databas senare
 var objPeople=[
     {
         username:"vigge",
@@ -22,24 +23,25 @@ var objPeople=[
         password:"windfew"
     }
     
-]
+];
 
 function getInfo(){
     //hämtar värdet på inputen av användaren under login fältet
-    var username = document.getElementById("username").value
-    var password = document.getElementById("password").value
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
     //info ifall elemnten hämtas eller inte, hittas i console
-    console.log("if the test passed the user name is " + username + " and the password is " +password)
+    console.log("if the test passed the user name is " + username + " and the password is " +password);
     
     for(i = 0; i < objPeople.length; i++){
         if(username == objPeople[i].username && password == objPeople[i].password)
         {
-            console.log(username + "is logged in")
-            return
+            console.log(username + "is logged in");
+            return;
+            // `?` antar att vi här får bygga upp en sida utifrån varje användare som victor nämnde
         }
         
     }
-    console.log("username or password didn't match")
+    console.log("username or password didn't match");
 }
 
 
