718f7d73e191972cd02bb4a5641340bccebf099d
Kalle <domare2@hotmail.com>
2021-05-04 02:11:49 +0200

    test av userLogin

---
+++ public_html/UserLogin.js
@@ -0,0 +1,34 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var objPeople=[
+    {
+        username:"vigge",
+        password:"Pilot"
+    },
+    {
+        username:"viktor",
+        password:"bean"
+    },
+    {
+        username:"preecha",
+        password:"ween"
+    },
+    {
+        username:"preecha",
+        password:"windfew"
+    }
+    
+]
+
+function getInfo(){
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+    console.log("if the test passed the user name is " + username + " and the password is " +password)
+}
+
+
+
+
