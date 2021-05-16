b13e8d732cf7f8a00ee2ad789e7d1187916d65b9
Preecha Häll <preecha.hall@gmail.com>
2021-05-16 16:45:03 +0200

    Update Sorting Files

--- public_html/assets/javascripts/createSide.js
+++ public_html/assets/javascripts/createSide.js
@@ -12,15 +12,15 @@ class tomsida {
         document.write('<html>');
         document.write('<head>');
         document.write('<meta name="viewport" content="width=device-width, initial-scale=1" >');
-        document.write('<link rel="stylesheet" href="w3.css">');
-        document.write('<link rel=icon href=SuF.png sizes="any" type="image/svg+xml">');
-        document.write('<script src="users.js"></script');
+        document.write('<link rel="stylesheet" href="./css/w3.css">');
+        document.write('<link rel=icon href=./images/SuF.png sizes="any" type="image/svg+xml">');
+        document.write('<script src="./assets/javascript/users.js"></script');
         document.write('<title>SUF</title>'); //Här kan man lägg in metod som automatiskt genererar titel
         document.write('</head>');
         //Sriver ut menyn högst upp och inkluderar logga
         document.write('<body style="background-color: #041414">');
         document.write('<header class="w3-container " style="background-color: " >');
-        document.write('<a href="Home.html"> <img src="SuF.png" height="70"></a>');
+        document.write('<a href="Home.html"> <img src="./images/SuF.png" height="70"></a>');
         document.write('</header>');
         //Inväntar metod för att generera iFrame antar jag?
         
@@ -29,7 +29,7 @@ class tomsida {
         //Skapar footer
         document.write('<footer class="w3-container " style="background-color: #041414">');
         document.write('<div class="logo ">');
-        document.write('<img  class="w3-left" src="SuF.png" alt="alt" height="138"/>');
+        document.write('<img  class="w3-left" src="./images/SuF.png" alt="alt" height="138"/>');
         document.write('<p class="w3-right" style="color: white">&copy; 2021 skoluniform </p>');
         document.write('</div');
         document.write('</footer>');
